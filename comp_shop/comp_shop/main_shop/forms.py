1074a65ef8cd5ae22519a90e3c2a33544ebc9082
Xolerik <72681052+Xolerik@users.noreply.github.com>
2021-06-29 21:13:03 +0700

    Исправлены опечатки в классе и удалено из сипска поле с датой

--- comp_shop/comp_shop/main_shop/forms.py
+++ comp_shop/comp_shop/main_shop/forms.py
@@ -2,11 +2,11 @@ from django.forms import ModelForm
 from .models import AMDVideo, AMDProcessor
 
 
-class AMDVideoForm(ModelForm)
+class AMDVideoForm(ModelForm):
     class Meta:
         # Модель, для которой делается форма
         model = AMDVideo
         # Последовательность имён полей, которые должны присутствовать в форме
         fields = ["full_name", "abbreviated_name", "price", "amount", "name_chipset", "line_chipset", "vram",
                   "vram_type",
-                  "directx", "video_partners", "date_added", "content", "product_section_amd_videcard"]
+                  "directx", "video_partners", "content", "product_section_amd_videcard"]
