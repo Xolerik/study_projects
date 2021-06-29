d56b4a504b1668eccfe44b9158601f54c72c895f
Xolerik <72681052+Xolerik@users.noreply.github.com>
2021-06-29 16:59:11 +0700

    Начальные url

--- comp_shop/comp_shop/main_shop/urls.py
+++ comp_shop/comp_shop/main_shop/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
-from .views import index, product_type, AMDVideoView
+from .views import index, product_type, AMDVideoView, video_cards
 
+urlpatterns = [
+    path("<int:product_id>/", product_type, name="producttype"),
+    path ('', index, name="index"),
+    path("create/", AMDVideoView.as_view(), name="create"),
+    path("video_cards/", video_cards, name="video_cards")
+]
