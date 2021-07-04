e216dff7aeb0c73248cc3e1cca6c19b06f88f4d6
Xolerik <72681052+Xolerik@users.noreply.github.com>
2021-07-04 20:28:25 +0700

    Небольшие изменения в контроллерах

--- comp_shop/comp_shop/main_shop/views.py
+++ comp_shop/comp_shop/main_shop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import AMDVideo, AMDVideoChipset, AMDLineVideoChipset, AMDProcessor, AMDProcessorSocket, \
     AMDProcessorChipsetName, ProductType
 from django.views.generic.edit import CreateView
+from django.views import generic
 from django.urls import reverse_lazy
 from .forms import AMDVideoForm
 
@@ -18,32 +19,44 @@ class AMDVideoView(CreateView):
     def get_context_data(self, **kwargs):
         """Функция для формирования контекста шаблона"""
         context = super().get_context_data(**kwargs)
-        context['producttypes'] = AMDVideoChipset.objects.all()
+        # Добавляется новая переменная к контексту и инициализируется некоторое значение
+        context['producttypes'] = ProductType.objects.all()
         return context
 
 
 def index(request):
     """Функция контроллер для стартовой страницы"""
     amdvideos = AMDVideo.objects.all()
-    video_chiposet = AMDLineVideoChipset.objects.all()
+    video_chipset = AMDLineVideoChipset.objects.all()
     producttypes = ProductType.objects.all()
     # Контекст шаблона, ключ - имя переменной по которой доступны данные в самом шаблоне,
     # значение - сами данные получамые из БД
-    context = {"amdvideos": amdvideos, "products": producttypes, "video_chiposet": video_chiposet}
+    context = {
+        "amdvideos": amdvideos,
+        "products": producttypes,
+        "video_chipset": video_chipset
+    }
     return render(request, "main_shop/index.html", context)
 
 
-def video_chipset(request, video_chipset_id):
-    """Функция контроллер для отображения чипсетов видеокарты"""
-    video_chipset = AMDLineVideoChipset.objects.filter(video_chipset=video_chipset_id)
-    current_video_chipset = AMDLineVideoChipset.objects.get(pk=video_chipset_id)
-    context = {"video_chipset": video_chipset}
-    return render(request, 'main_shop/video_chipset.html', context)
+class AMDVideoChipsetView(generic.ListView):
+    """Класс-контроллер для отображения чипсетов видеокарт"""
+    # Модель, для объектов которой требуется вывод
+    model = AMDVideoChipset
+    # Настройка постраничного вывода, в данном случае - 10 объектов на страницу
+    paginate_by = 10
+
+# def video_chipset(request, video_chipset_id):
+#     """Функция контроллер для отображения чипсетов видеокарты"""
+#     video_chipset = AMDLineVideoChipset.objects.filter(video_chipset=video_chipset_id)
+#     current_video_chipset = AMDLineVideoChipset.objects.get(pk=video_chipset_id)
+#     context = {"video_chipset": video_chipset}
+#     return render(request, 'main_shop/video_chipset.html', context)
 
 def video_cards(request):
     """Функция контроллер для отображения списка видеокарт"""
-    video_chiposet = AMDLineVideoChipset.objects.all()
-    context = {"video_chiposet": video_chiposet}
+    video_chipset = AMDLineVideoChipset.objects.all()
+    context = {"video_chipset": video_chipset}
     return render(request, "main_shop/video_cards.html", context)
 
 def product_type(request, product_id):
@@ -52,5 +65,8 @@ def product_type(request, product_id):
     producttypes = ProductType.objects.filter(product=product_id)
     # Выбор всех типов продуктов
     current_producttype = ProductType.objects.get(pk=product_id)
-    context = {"producttypes": producttypes, "current_producttype": current_producttype}
+    context = {
+        "producttypes": producttypes,
+        "current_producttype": current_producttype
+    }
     return render(request, 'main_shop/product_types.html', context)
