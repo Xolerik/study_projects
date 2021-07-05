01af04f2a9f37157879898f78e8d9e70432692b5
Xolerik <72681052+Xolerik@users.noreply.github.com>
2021-07-05 15:54:42 +0700

    Настройки директорий к Media и Static, а так же их геренрация URL

--- comp_shop/shop_v2/shop_v2/settings.py
+++ comp_shop/shop_v2/shop_v2/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,7 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+#URL по которому доступны статик файлы
 STATIC_URL = '/static/'
+# Путь до папки статик файлов, от корня проекта
+STATIC_ROOT = BASE_DIR/'static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
+# Директории, где джанго ищет новые файлы
+STATICFILES_DIRS = [
+    BASE_DIR/'static_dev',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
