f9dd535545f1f94e5e2a7d3b6e1aee921a6b7a7f
Xolerik <72681052+Xolerik@users.noreply.github.com>
2021-07-05 16:11:44 +0700

    Created 2 models: Notebook and Smartphone. Created LatestProductManager(скорее всего перепишу данную конструкцию во вьюшке приложения)

--- comp_shop/shop_v2/mainapp/models.py
+++ comp_shop/shop_v2/mainapp/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+# ContentType - мини фреймворк, который видит все модели из приложений, которые есть в Installed_apps в settings
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Параметры берутся из settings.AUTH_USER_MODEL (само поле скрыто, но есть значения по умолчанию)
 User = get_user_model()
+
+
+class LatestProductsManager:
+    """Очень сомнительный класс, следует переписать код в файле view применяя пагинатор"""
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        return products
+
+
+class LatestProducts:
+    objects = LatestProductsManager
 
 
 class Category(models.Model):
@@ -28,12 +46,48 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        abstract = True
+
+
+class Notebook(Product):
+    """Ноутбуки"""
+    diagonal = models.CharField(max_length=50, verbose_name="Диагональ")
+    display_type = models.CharField(max_length=50, verbose_name="Тип дисплэя")
+    resolution = models.CharField(max_length=100, verbose_name="Разрешение экрана")
+    processor_freq = models.CharField(max_length=50, verbose_name="Частота процессора")
+    ram = models.CharField(max_length=100, verbose_name="Оперативная память")
+    video = models.CharField(max_length=100, verbose_name="Видеокарта")
+    time_without_charge = models.CharField(max_length=5, verbose_name="Время работы от аккумулятора")
+
+    def __str__(self):
+        return f"{self.category.name} : {self.title}"
+
+
+class Smartphone(Product):
+    """Смартфоны"""
+    diagonal = models.CharField(max_length=50, verbose_name="Диагональ")
+    display_type = models.CharField(max_length=50, verbose_name="Тип дисплэя")
+    resolution = models.CharField(max_length=100, verbose_name="Разрешение экрана")
+    accum_value = models.CharField(max_length=100, verbose_name="Объем аккумулятора")
+    ram = models.CharField(max_length=100, verbose_name="Оперативная память")
+    # Возможность добавлять внешнюю память
+    sd = models.BooleanField(default=True)
+    sd_value_max = models.CharField(max_length=100, verbose_name="Максимальный объем встраиваемой памяти")
+    main_cam_mp = models.CharField(max_length=20, verbose_name="Главная камера")
+    frontal_cam_mp = models.CharField(max_length=20, verbose_name="Фронтальная камера")
+
+    def __str__(self):
+        return f"{self.category.name} : {self.title}"
+
 
 class CartProduct(models.Model):
     """Заглушка - продукт корзины"""
     user = models.ForeignKey("Customer", verbose_name="Покупатель", on_delete=models.CASCADE)
     cart = models.ForeignKey("Cart", verbose_name="Корзина", on_delete=models.CASCADE, related_name="related_products")
-    product = models.ForeignKey("Product", verbose_name="Товар", on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
     # Количество
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая стоимость")
@@ -50,7 +104,7 @@ class Cart(models.Model):
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая стоимость")
 
     def __str__(self):
-        return f""
+        return f"Корзина: {self.owner}"
 
 
 class Customer(models.Model):
@@ -61,13 +115,3 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"Покупатель: {self.user.first_name} {self.user.last_name}"
-
-
-class Specifications(models.Model):
-    """Характеристики товара"""
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name="Имя товара для характеристик")
-
-    def __str__(self):
-        return f"Характеристики для {self.name}"
