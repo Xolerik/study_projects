a003fd32c7ac6ce790d2ef010c0fdadca25ec528
Xolerik <72681052+Xolerik@users.noreply.github.com>
2021-07-06 16:27:25 +0700

    Функция заглушка

--- comp_shop/shop_v2/mainapp/views.py
+++ comp_shop/shop_v2/mainapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    # """Функция контроллер для стартовой страницы"""
+    # amdvideos = AMDVideo.objects.all()
+    # video_chipset = AMDLineVideoChipset.objects.all()
+    # producttypes = ProductType.objects.all()
+    # # Контекст шаблона, ключ - имя переменной по которой доступны данные в самом шаблоне,
+    # # значение - сами данные получамые из БД
+    # context = {
+    #     "amdvideos": amdvideos,
+    #     "products": producttypes,
+    #     "video_chipset": video_chipset
+    # }
+    # return render(request, "main_shop/index.html", context)
+    return render(request, "mainapp/base.html", {})
