e3917aa2b61bd1f7c001adc1f8f4342cd51c9f72
Xolerik <72681052+Xolerik@users.noreply.github.com>
2021-07-09 16:49:01 +0700

    Доделаны url маршруты с использованием slug-ов

--- comp_shop/shop_v2/mainapp/urls.py
+++ comp_shop/shop_v2/mainapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, ProductDetailView
+from .views import index, ProductDetailView, Smartphone
 
 urlpatterns = [
     path('', index, name="base" ),
-    path('products/<str:slug>/', ProductDetailView.as_view(), name="product_detail" ),
+    path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name="product_detail" ),
+    # path('products/<str:slug>/', ProductDetailView.as_view(), name="product_detail" ),
 ]
