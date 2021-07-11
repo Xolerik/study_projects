a8c19ec1fa83ba7bebd4eb1c1c7cb0a60b77be84
Xolerik <72681052+Xolerik@users.noreply.github.com>
2021-07-11 21:03:44 +0700

    Default folder

--- comp_shop/shop_v2/mainapp/views.py
+++ comp_shop/shop_v2/mainapp/views.py
@@ -3,19 +3,8 @@ from django.views.generic import DetailView, CreateView
 from .models import Product, Smartphone, Notebook
 # Create your views here.
 def index(request):
-    # """Функция контроллер для стартовой страницы"""
-    # amdvideos = AMDVideo.objects.all()
-    # video_chipset = AMDLineVideoChipset.objects.all()
-    # producttypes = ProductType.objects.all()
-    # # Контекст шаблона, ключ - имя переменной по которой доступны данные в самом шаблоне,
-    # # значение - сами данные получамые из БД
-    # context = {
-    #     "amdvideos": amdvideos,
-    #     "products": producttypes,
-    #     "video_chipset": video_chipset
-    # }
-    # return render(request, "main_shop/index.html", context)
-    return render(request, "mainapp/base.html", {})
+
+    return render(request, "layout/base.html", {})
 
 class ProductDetailView(DetailView):
 
