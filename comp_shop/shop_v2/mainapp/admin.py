231739b97ef29e104beafa59ca5d8ede4912cceb
Xolerik <72681052+Xolerik@users.noreply.github.com>
2021-07-12 16:38:18 +0700

    Функционал для обработки boolean поля

--- comp_shop/shop_v2/mainapp/admin.py
+++ comp_shop/shop_v2/mainapp/admin.py
@@ -1,6 +1,27 @@
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm
 from django.contrib import admin
 from .models import *
+
+
+class SmartphoneAdminForm(ModelForm):
+    """Отрисовка флажка SD карты при рендеринге формы"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Обращение к экземпляру формы
+        instance = kwargs.get('instance')
+        # Проверка чекбокса sd и переопределение атрибутов виджета для поля sd_volume_max
+        if not instance.sd:
+            self.fields['sd_volume_max'].widget.attrs.update({
+                'readonly': True, 'style ': 'background: lightgrey'
+            })
+
+    def clean(self):
+        """Метод для работы с полями формы"""
+        # Условия для пустого чекбокса sd карты
+        if not self.cleaned_data['sd']:
+            self.cleaned_data['sd_volume_max'] = None
+        return self.cleaned_data
+
 
 class NotebookAdmin(admin.ModelAdmin):
     """Отображение категории в админке"""
@@ -17,13 +38,14 @@ class NotebookAdmin(admin.ModelAdmin):
 class SmartphoneAdmin(admin.ModelAdmin):
     """Отображение категории в админке"""
     list_display = ("title", "price", "diagonal", "resolution")
+    # Шаблон для изменения стандартного шаблона админки
+    change_form_template = 'mainapp/admin.html'
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         # Сравнение поля из БД
         if db_field.name == "category":
             return ModelChoiceField(Category.objects.filter(slug="smartphones"))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
 
 
 admin.site.register(Category)
