6e8743879714c316ae8b3c678ed63d7c679be58d
Xolerik <72681052+Xolerik@users.noreply.github.com>
2021-07-19 16:29:54 +0700

    Добавлены url адреса для корзины и для добавление товара в корзину.

--- comp_shop/shop_v2/mainapp/urls.py
+++ comp_shop/shop_v2/mainapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ProductDetailView, Smartphone, CategoryDetailView, BaseView
+from .views import ProductDetailView, Smartphone, CategoryDetailView, BaseView, CartView, AddToCartView
 
 urlpatterns = [
     path('', BaseView.as_view(), name="base" ),
     path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name="product_detail" ),
     path('category/<str:slug>', CategoryDetailView.as_view(), name="category_detail" ),
+    path('cart/', CartView.as_view(), name="cart" ),
+    path('add_to_cart/<str:ct_model>/<str:slug>', AddToCartView.as_view(), name="add_to_cart" ),
 ]
