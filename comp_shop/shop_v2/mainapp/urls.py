011f62dbfaa39ab216c21df740ee488464a2cc49
Ustinov Denis <xolerik241@gmail.com>
2021-08-05 19:34:06 +0700

    1. Cosmetic changes 2. Add new urls path for remove product from cart and change qty in cart

--- comp_shop/shop_v2/mainapp/urls.py
+++ comp_shop/shop_v2/mainapp/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import ProductDetailView, Smartphone, CategoryDetailView, BaseView, CartView, AddToCartView
+from .views import (
+    ProductDetailView,
+    CategoryDetailView,
+    BaseView,
+    CartView,
+    AddToCartView,
+    DeleteFromCartView,
+    ChangeQTYView
+)
 
 urlpatterns = [
-    path('', BaseView.as_view(), name="base" ),
-    path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name="product_detail" ),
-    path('category/<str:slug>', CategoryDetailView.as_view(), name="category_detail" ),
-    path('cart/', CartView.as_view(), name="cart" ),
-    path('add_to_cart/<str:ct_model>/<str:slug>', AddToCartView.as_view(), name="add_to_cart" ),
+    path('', BaseView.as_view(), name="base"),
+    path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name="product_detail"),
+    path('category/<str:slug>', CategoryDetailView.as_view(), name="category_detail"),
+    path('cart/', CartView.as_view(), name="cart"),
+    path('add_to_cart/<str:ct_model>/<str:slug>', AddToCartView.as_view(), name="add_to_cart"),
+    path('remove_from_cart/<str:ct_model>/<str:slug>', DeleteFromCartView.as_view(), name="delete_from_cart"),
+    path('change_qty/<str:ct_model>/<str:slug>', ChangeQTYView.as_view(), name="change_qty"),
 ]
