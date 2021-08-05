206f763875db894b7bd826dce90443d7c6484a1f
Ustinov Denis <xolerik241@gmail.com>
2021-08-05 19:43:02 +0700

    1. Add ChangeQTYView functionality for changing the quantity of products in the cart 2. Add DeleteFromCartView functionality for remove products from cart

--- comp_shop/shop_v2/mainapp/views.py
+++ comp_shop/shop_v2/mainapp/views.py
@@ -3,10 +3,11 @@ from django.http import HttpResponseRedirect
 from django.contrib.contenttypes.models import ContentType
 from django.views.generic import DetailView, CreateView, View
 from .models import Product, Smartphone, Notebook, Category, LatestProducts, Customer, Cart, CartProduct
-from .mixins import CategoryDetailMixin
+from .mixins import CategoryDetailMixin, CartMixin
+from django.contrib import messages
 
 
-class BaseView(View):
+class BaseView(CartMixin, View):
     """Базовая страница"""
 
     def get(self, request, *args, **kwargs):
@@ -14,12 +15,37 @@ class BaseView(View):
         products = LatestProducts.objects.get_products_for_main_page('notebook', 'smartphone')
         context = {
             'categories': categories,
-            'products': products
+            'products': products,
+            'cart': self.cart
         }
         return render(request, "layout/base.html", context)
 
 
-class ProductDetailView(CategoryDetailMixin, DetailView):
+class ChangeQTYView(CartMixin, View):
+    """Изменение количества товара в корзине"""
+
+    def post(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        # Определяем модель для товара
+        content_type = ContentType.objects.get(model=ct_model)
+        # Получаем продукт обращаясь через content_type к родительской модели.
+        # Далее через менеджер(objects) получаем продук по product_slug
+        product = content_type.model_class().objects.get(slug=product_slug)
+        # Получаем корзину с указанными параметрами
+        cart_product = CartProduct.objects.get(
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id,
+        )
+        qty = int(request.POST.get('qty'))
+        cart_product.qty = qty
+        cart_product.save()
+        self.cart.save()
+        messages.add_message(request, messages.INFO, "Количество изменено")
+        return HttpResponseRedirect('/shop/cart/')
+
+
+class ProductDetailView(CartMixin, CategoryDetailMixin, DetailView):
+    """Детальная информация о продукте"""
+
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
         'smartphone': Smartphone
@@ -39,10 +65,12 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
         context = super().get_context_data(**kwargs)
         # Получение имени модели из скрытого метода _meta
         context['ct_model'] = self.model._meta.model_name
+        # Добавление корзины в шаблон
+        context['cart'] = self.cart
         return context
 
 
-class CategoryDetailView(CategoryDetailMixin, DetailView):
+class CategoryDetailView(CartMixin, CategoryDetailMixin, DetailView):
     """Детальная информация по категориям"""
     model = Category
     queryset = Category.objects.all()
@@ -50,39 +78,69 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
     template_name = 'mainapp/category_detail.html'
     slug_url_kwarg = 'slug'
 
+    def get_context_data(self, **kwargs):
+        """Получение контекста модели"""
+        context = super().get_context_data(**kwargs)
+        # Добавление корзины в шаблон
+        context['cart'] = self.cart
+        return context
 
-class AddToCartView(View):
-    """Добавление товара в корзину"""
+
+class AddToCartView(CartMixin, View):
+    """Добавление товара в корзин"""
 
     def get(self, request, *args, **kwargs):
         ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
-        # Получаем текущего пользователя
-        customer = Customer.objects.get(user=request.user)
-        # Получаем корзину привязываемую к пользователяю, in_order - активная только одна
-        cart = Cart.objects.get(owner=customer, in_order=False)
         # Определяем модель для товара
         content_type = ContentType.objects.get(model=ct_model)
         # Получаем продукт обращаясь через content_type к родительской модели.
         # Далее через менеджер(objects) получаем продук по product_slug
         product = content_type.model_class().objects.get(slug=product_slug)
-        cart_product = CartProduct.objects.create(
-            user=cart.owner, cart=cart, content_object=product, final_price=product.price
+        # На основании полученных данных создаём корзину для текущего покупателя
+        # get_or_create возвращает кортеж из двух значений, где первое  - сам объект(полученный или созданный),
+        # второе - булевое значение (был создан или нет).
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id,
         )
-        cart.products.add(cart_product)
+        if created:
+            self.cart.products.add(cart_product)
+        self.cart.save()
+        messages.add_message(request, messages.INFO, "Товар успешно добавлен")
         # Редирект в корзину, разделители в данном случае КРАЙНЕ важны для построение url адреса.
         return HttpResponseRedirect('/shop/cart/')
 
 
-class CartView(View):
+class DeleteFromCartView(CartMixin, View):
+    """Удаление товара из корзины"""
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        # Определяем модель для товара
+        content_type = ContentType.objects.get(model=ct_model)
+        # Получаем продукт обращаясь через content_type к родительской модели.
+        # Далее через менеджер(objects) получаем продук по product_slug
+        product = content_type.model_class().objects.get(slug=product_slug)
+        # Получаем корзину с указанными параметрами
+        cart_product = CartProduct.objects.get(
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id,
+        )
+        self.cart.products.remove(cart_product)
+        # Удаление корзины с продуктами
+        cart_product.delete()
+        self.cart.save()
+        messages.add_message(request, messages.INFO, "Товар успешно удалён")
+        # Редирект в корзину, разделители в данном случае КРАЙНЕ важны для построение url адреса.
+        return HttpResponseRedirect('/shop/cart/')
+
+
+class CartView(CartMixin, View):
     """Рендеринг корзины"""
 
     def get(self, request, *args, **kwargs):
         # Берётся текущий пользователь
-        customer = Customer.objects.get(user=request.user)
-        cart = Cart.objects.get(owner=customer)
         categories = Category.objects.get_categories_for_left_sidebar()
         context = {
-            'cart': cart,
+            'cart': self.cart,
             'categories': categories
         }
         return render(request, 'mainapp/cart.html', context)
