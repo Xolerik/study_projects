5a563a15ba45d2e63ce67e944e91733dc2298aa2
Ustinov Denis <xolerik241@gmail.com>
2021-08-10 21:43:34 +0700

    Add django-crispy-forms

--- comp_shop/shop_v2/shop_v2/settings.py
+++ comp_shop/shop_v2/shop_v2/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp.apps.MainappConfig'
+    'mainapp.apps.MainappConfig',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
