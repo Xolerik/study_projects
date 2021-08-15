a08e822498c2d84833739a03aee02fca56e7d61d
Ustinov Denis <xolerik241@gmail.com>
2021-08-15 19:31:20 +0700

    Add LOGIN_REDIRECT_URL and LOGOUT_REDIRECT_URL

--- comp_shop/shop_v2/shop_v2/settings.py
+++ comp_shop/shop_v2/shop_v2/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure--=jbhon1&_z3stti641imou50d#3e$gi(yph-_ue(yiw82%*ra
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shop_v2.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -85,7 +82,6 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -119,21 +114,20 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-#URL по которому доступны статик файлы
+# URL по которому доступны статик файлы
 STATIC_URL = '/static/'
 # Путь до папки статик файлов, от корня проекта
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Директории, где джанго ищет новые файлы
 STATICFILES_DIRS = [
-    BASE_DIR/'static_dev',
+    BASE_DIR / 'static_dev',
 ]
 
 # Default primary key field type
@@ -141,5 +135,8 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Перенаправление пользователя на страницу после успешного входа
+LOGIN_REDIRECT_URL = 'base'
+LOGOUT_REDIRECT_URL = 'base'
