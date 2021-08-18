c5d0acf5fdb6ae207dad78c284cc640b7a4d020d
Ustinov Denis <xolerik241@gmail.com>
2021-08-18 23:19:22 +0700

    Add RegisterUserForm

--- comp_shop/shop_v2/mainapp/forms.py
+++ comp_shop/shop_v2/mainapp/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Order
 
@@ -17,3 +19,15 @@ class OrderForm(forms.ModelForm):
         fields = (
             'first_name', 'last_name', 'phone', 'address', 'buying_type', 'order_date', 'comment',
         )
+
+class RegisterUserForm(UserCreationForm):
+    """Форма регистрации пользователя"""
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', 'email')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-input'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
+            'email': forms.EmailInput(attrs={'class': 'form-input'}),
+        }
