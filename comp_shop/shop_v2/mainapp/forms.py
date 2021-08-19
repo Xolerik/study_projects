5c44fabedf5057d277e030b68a79c4f1329526c0
Ustinov Denis <xolerik241@gmail.com>
2021-08-19 22:09:43 +0700

    Add fields in RegisterUserForm And beta class AuthUserForm

--- comp_shop/shop_v2/mainapp/forms.py
+++ comp_shop/shop_v2/mainapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from .models import Order
 
@@ -22,12 +22,30 @@ class OrderForm(forms.ModelForm):
 
 class RegisterUserForm(UserCreationForm):
     """Форма регистрации пользователя"""
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Введите пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2', 'email')
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-input'}),
+            'username': forms.TextInput(),
             'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
             'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
             'email': forms.EmailInput(attrs={'class': 'form-input'}),
         }
+
+# class AuthUserForm(AuthenticationForm):
+#     """Форма входа пользователя"""
+#     username = forms.CharField(label='Введите имя пользователя: ', widget=forms.TextInput(attrs={'class': 'form-input'}))
+#     password = forms.CharField(label='Введите пароль для входа: ', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+#
+#     class Meta:
+#         model = User
+#         fields = ('username', 'password1')
+#         widgets = {
+#             'username': forms.TextInput(),
+#             'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
+#         }
